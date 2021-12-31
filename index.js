aba5eb87fdaca6e47d6e04ec9426cbbb797d9a9e
Abel.Tomas.16 <abeljosetoms115@gmail.com>
2021-12-31 05:25:46 +0100

    FEAT: pegar porta do heroku

--- index.js
+++ index.js
@@ -1,8 +1,9 @@
 const htpp = require('http')
+const port = process.env.PORT || 4000
 const handler = (req, res) => {
     res.writeHead(200, { 'content-type': 'text/plain' })
     res.end("Olá, bem vindo ao socket");
 }
 const server = htpp.createServer(handler)
 require('./Service/socket')(server)
-server.listen(80, "0.0.0.0")
+server.listen(port, "0.0.0.0")
